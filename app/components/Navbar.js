988ef812ae9d2027ae12cf39e9e405d26309813b
jaiswaldhiraj <jaiswaldhiraj928@gmail.com>
2025-08-30 21:56:31 +0530

    link box drop down problem fiex

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -14,22 +14,23 @@ const Navbar = () => {
 
         <div id="nav_logo" className='ml-8 text-2xl'>Dhiraj's Portfolio</div>
 
-        <div id="nav_links" className='mr-12 text-l'>
+        <div id="nav_links" className='text-l'>
           <button
-            className="text-white md:hidden"
+            className="text-white md:hidden mr-10"
             onClick={() => setOpen(!open)}
           >
             ☰
           </button>
 
-          <ul
-            className={`${open ? "block" : "hidden"
-              } md:flex md:space-x-6 gap-8 text-xl`}
-          >
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
-          </ul>
+<ul
+  className={`${
+    open ? "block" : "hidden"
+  } md:m-0 md:p-0 md:flex md:space-x-6 gap-8 text-xl mt-31`}
+>
+  <li><Link href="/">Home</Link></li>
+  <li><Link href="/about">About</Link></li>
+  <li><Link href="/contact">Contact</Link></li>
+</ul>
 
         </div>
       </nav>
