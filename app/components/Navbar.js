529e35a59cf02e53ccbeafae2b4019a1bdb0d070
jaiswaldhiraj <jaiswaldhiraj928@gmail.com>
2025-08-30 21:22:43 +0530

    navbar responsive added

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,25 +1,38 @@
+"use client"
+
 import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+
   return (
-    <> 
-    <nav className='flex items-center justify-between h-16 text-blue-50 bg-black'>
+    <>
+      <nav className='flex items-center justify-between h-16 text-blue-50 bg-black'>
 
         <div id="nav_logo" className='ml-8 text-2xl'>Dhiraj's Portfolio</div>
 
         <div id="nav_links" className='mr-12 text-l'>
+          <button
+            className="text-white md:hidden"
+            onClick={() => setOpen(!open)}
+          >
+            ☰
+          </button>
 
-            <ul className='flex gap-8 text-1xl'>
+          <ul
+            className={`${open ? "block" : "hidden"
+              } md:flex md:space-x-6 gap-8 text-xl`}
+          >
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/contact">Contact</Link></li>
+          </ul>
 
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-
-
-            </ul>
         </div>
-    </nav>
+      </nav>
     </>
   )
 }
